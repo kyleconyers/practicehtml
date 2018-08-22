714b680c10e444cf99774b6fdb755cb576dbe4af
kyleconyers <conyersky@gmail.com>
2018-08-21 21:59:10 -0700

    first commmit

---
+++ hangman.js
@@ -0,0 +1,56 @@
+//pseudocode review
+
+// use cases:
+// use 1: The user loads the page
+// user knows how to use game
+    //display underscores according to randomly chosen words
+    // display guess remaining, wins, and losses
+//use #2. user clicks a lteer between a-z
+//if the user guesses a letter thats in the word, display the letter in its approporiate position, else guesses go down remaining go down by 1. if guesses remaining equal 0, user loses.
+// if the user guesses all the correct letters in the word, the win.
+//increase wins by 1
+
+
+//============= variables ==============//
+
+//var wordBank = [array]
+//wins and losses
+// var wins
+// var losses
+// var guessesleft =10
+
+
+//var wrongGuesses =[]
+// store user input from keyboard event
+//var userInput; 
+//randomly chosen word form our wordbank
+//varcurrentword
+//var drawnword =[]
+//var underscores =[]
+
+
+
+
+
+
+//============= functions===========//
+// on page load execute or reset function
+//reset()
+//math.floor and math.random to randomly choose a word form the wordBank
+//ex. currentWord = 'seattle'
+//define length of current word
+//add underscores to the underscores array according to the length of the word
+// we need to write syntax using currentWord as our input and this should outpout an arry of underscores according to the length of the current word
+
+//--- output ["_","_","_","_","_","_","_","_","_","_","_",]
+
+// select the html and display our underscores
+// display guessremaining, wins, and losses
+// function ({})
+
+
+
+
+
+// ================= event listeners============//
+
